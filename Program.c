670a1a30ce49a77f25d632156260696e035a18a0
Sunl1t <mr.serzh.smekalov@mail.ru>
2023-11-13 17:39:07 +0300

    Создано меню

---
+++ Program.c
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        char ch1 = ' '; // переменная для хранения первого символа
+        char ch2 = ' '; // переменная для хранения второго символа
+        while (true)
+        {
+            Console.WriteLine("Меню выбора:");
+            Console.WriteLine("1. Ввести первый символ");
+            Console.WriteLine("2. Ввести второй символ");
+            Console.WriteLine("3. Вывести каждый символ и его код ASCII");
+            Console.WriteLine("4. Вывести сумму кодов ASCII символов ch1 и ch2");
+            Console.WriteLine("0. Выйти");
+
+            Console.Write("Выберите пункт меню: ");
+        }
+    }
+}
