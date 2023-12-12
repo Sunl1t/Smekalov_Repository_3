24acbd02a7cc0f6938df8cd0a728bef1b11f9a80
Sunl1t <145154426+Sunl1t@users.noreply.github.com>
2023-12-12 18:18:07 +0300

    Add files via upload

---
+++ Build.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\
+VCExpress.exe" project_test.sln /build Debug
+"C:/Users/mrser/OneDrive/Рабочий стол/DoxyGen\bin\doxygen.exe" myConfig
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" build.iss
